b7ef021e3eb8ffe52332a9ccd3bac8c4e5b3fcef
FengyiQuan <quan.f@husky.neu.edu>
2021-04-11 04:01:19 +0800

    up

--- frozen.py
+++ frozen.py
@@ -1,26 +1,29 @@
 import gym
+import plot
 from QLearningAgent import QLearningAgent
 from DynaQAgent import DynaQAgent
 from DeepQLearningAgent import DeepQLearningAgent
+
 env = gym.make('FrozenLake8x8-v0')
 env.reset()
 
 # env.reset()
 # env.render()
-epsilon = 0.9
-lr_rate = 0.1
+epsilon = 0.5
+lr_rate = 0.3
 gamma = 0.96
 step = 6
-total_episodes = 10000
+total_episodes = 1000
 max_steps = 1000
-
 
 qLearning = QLearningAgent(env, epsilon, gamma, lr_rate)
 dynaq = DynaQAgent(env, epsilon, gamma, lr_rate, step)
-dQ = DeepQLearningAgent(env,epsilon,gamma,lr_rate)
+dQ = DeepQLearningAgent(env, epsilon, gamma, lr_rate)
+
 
 def train(agent):
     win = 0
+    scores = []
     for ep in range(total_episodes):
         state = env.reset()
         t = 0
@@ -34,9 +37,12 @@ def train(agent):
             s += reward
             if done:
                 break
-        if s >= 1 : win += 1
-        if ep % 10 == 0:print('Episode', ep, 'Takes',t ,'steps','score:', s)
-    print ('Win: ',win)
+        if s >= 1: win += 1
+        if ep % 10 == 0: print('Episode', ep, 'Takes', t, 'steps', 'score:', s)
+        scores.append(s)
+    print('Win: ', win)
+    return scores
+
 
 def play(agent, numberEpisode=1000):
     win = 0
@@ -45,24 +51,27 @@ def play(agent, numberEpisode=1000):
         t = 0
         score = 0
         while t < 100:
-            #env.render()
+            # env.render()
             action = agent.choose_play_action(state)
             next_state, reward, done, info = env.step(action)
             state = next_state
-            t+=1
-            score +=reward
+            t += 1
+            score += reward
             if done:
                 break
         if score >= 1:
             win += 1
-    print('Win: ',win)
+    print('Win: ', win)
+
+
 #
 # train(qLearning)
 # play(qLearning,1000)
 # train(dynaq)
 # play(dynaq,1000)
-train(dQ)
-play(dQ,1000)
+win = train(dQ)
+play(dQ, 1000)
+plot.plot_rwd_by_epd(win)
 #
 # with open("frozenLake_qTable.pkl", 'wb') as f:
 #     pickle.dump(QLearningAgent.Q, f)
@@ -70,5 +79,4 @@ play(dQ,1000)
 # with open("frozenLake_qTable.pkl", 'rb') as f:
 # 	QLearningAgent.Q = pickle.load(f)
 
-#print(QLearningAgent.Q)
-
+# print(QLearningAgent.Q)
---
+++ plot.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_rwd_by_epd(rewards, epd_range=300):
+    sum_rewards = []
+    index = []
+    for i in range(0, len(rewards), epd_range):
+        index.append(i)
+        sum_rewards.append(sum(rewards[i:i + epd_range]))
+
+    plt.plot(index, sum_rewards)
+    plt.ylabel('sum reward')
+    plt.xlabel('episodes')
+    plt.show()
