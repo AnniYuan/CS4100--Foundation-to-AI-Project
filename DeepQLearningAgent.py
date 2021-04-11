58f413c2ac0e7793b540dc1994194b5942cc1661
FengyiQuan <quan.f@husky.neu.edu>
2021-04-12 00:55:06 +0800

    up

--- DeepQLearningAgent.py
+++ DeepQLearningAgent.py
@@ -23,25 +23,22 @@ class DeepQLearningAgent:
         self.init = tf.global_variables_initializer()
         self.sess.run(self.init)
 
-    def one_hot(self, state):
-        action, qvalues = self.sess.run([self.act, self.out],
-                                        feed_dict={self.x: np.identity(self.n_obs)[state:state + 1]})
-        return action, qvalues
+    def one_hot_encode(self, state):
+        return np.identity(self.n_obs)[state:state + 1]
 
     def choose_action(self, state):
-        action, qvalues = self.one_hot(state)
+        action, qvalues = self.sess.run([self.act, self.out], feed_dict={self.x: self.one_hot_encode(state)})
         self.q_predict = qvalues
         if np.random.uniform(0, 1) < self.epsilon:
             action[0] = self.env.action_space.sample()
         return action[0]
 
     def choose_play_action(self, state):
-        action, qvalues = self.one_hot(state)
+        action, qvalues = self.sess.run([self.act, self.out], feed_dict={self.x: self.one_hot_encode(state)})
         return action[0]
 
     def update(self, state, next_state, reward, action):
-        qnext_values = self.sess.run([self.out], feed_dict={self.x: np.identity(self.n_obs)[next_state:next_state + 1]})
+        qnext_values = self.sess.run([self.out], feed_dict={self.x: self.one_hot_encode(next_state)})
         targetq = self.q_predict
         targetq[0, action] = reward + self.gamma * np.max(qnext_values)
-        self.sess.run([self.train_step],
-                      feed_dict={self.x: np.identity(self.n_obs)[state:state + 1], self.t: targetq})
+        self.sess.run([self.train_step], feed_dict={self.x: self.one_hot_encode(state), self.t: targetq})
